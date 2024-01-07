a207af0e7821359a236b4bca7d4e0402e1794f20
Swastik Mishra <m.swastik@gmail.com>
2024-01-07 19:42:28 +0530

    Update depapp.py

--- depapp.py
+++ depapp.py
@@ -7,7 +7,6 @@ from datetime import datetime
 from dateutil.relativedelta import relativedelta
 import polars as pl
 import param
-from misc import options,css,fran,coun,em
 
 cwd=""
 today=datetime.today()
@@ -24,8 +23,8 @@ unv=pn.widgets.TextInput(name="User Name")
 psv=pn.widgets.PasswordInput(name="Password")
 sab=pn.widgets.Button(name="Save",button_type='primary')
 fcs=pn.widgets.Select(name="Forecast Type",options=['Stat Fcst','DF Fcst'],height=40,width=130)
-co=pn.widgets.Select(name="Select Country",options=coun,value='INDIA',height=40)
-fr=pn.widgets.Select(name='Select Franchise',options=fran,value='Joint Replacement',height=40)
+co=pn.widgets.Select(name="Select Country",options=['INDIA','CHINA'],value='INDIA',height=40)
+fr=pn.widgets.Select(name='Select Franchise',options=['ddd','Joint Replacement'],value='Joint Replacement',height=40)
 
 w3=pn.Row(unv,psv,sab,fcs,height=50).servable()
 temp.main.append(w3)
